d4e743b1a9add82a7c02ad5ea34fff850301b89f
Vladimir <vladimir.golovkin16@yandex.ru>
2021-10-15 21:10:05 +0500

    A, B, C, D

--- Matrix.py
+++ Matrix.py
@@ -1,7 +1,14 @@
-class Matrix:
-    # A
+import functools
+
+
+class _Matrix(object):
     def __init__(self, data):
         self.data = data.copy()
+
+
+class Matrix(_Matrix):
+
+    # A
 
     def __str__(self):
         matrix_str = ""
@@ -25,11 +32,39 @@ class Matrix:
                        for i in range(len(main_data))]
         return Matrix(result_data)
 
+    # D
+    @functools.singledispatchmethod
     def __mul__(self, other):
+        return NotImplemented
+
+    @__mul__.register(float)
+    def _(self, other):
+        return self.__mul_scalar(other)
+
+    @__mul__.register(int)
+    def _(self, other):
+        return self.__mul_scalar(other)
+
+    @__mul__.register(_Matrix)
+    def _(self, other):
+        main_size = self.size()
+        other_size = other.size()
+        if main_size[1] != other_size[0]:
+            raise MatrixError(self, other)
+
         main_data = self.data
-        result_data = [[main_data[i][j] * other for j in range(len(main_data[0]))]
-                       for i in range(len(main_data))]
-        return Matrix(result_data)
+        other_data = other.data
+        result = []
+        for i in range(main_size[0]):
+            result_row = []
+            for j in range(other_size[1]):
+                cell_sum = 0
+                for k in range(other_size[0]):
+                    cell_sum += main_data[i][k] * other_data[k][j]
+                result_row.append(cell_sum)
+            result.append(result_row)
+
+        return Matrix(result)
 
     def __rmul__(self, other):
         return self * other
@@ -42,6 +77,12 @@ class Matrix:
     def transposed(matrix):
         data = matrix.__transpose_data()
         return Matrix(data)
+
+    def __mul_scalar(self, other):
+        main_data = self.data
+        result_data = [[main_data[i][j] * other for j in range(len(main_data[0]))]
+                       for i in range(len(main_data))]
+        return Matrix(result_data)
 
     def __transpose_data(self):
         main_data = self.data
@@ -71,9 +112,9 @@ print(m.size())
 # B
 m1 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 m2 = Matrix([[0, 1, 0], [20, 0, -1], [-1, -2, 0]])
-print(f"Результат сложения:\n{m1 + m2}")
-print(f"Результат умножения на скаляр:\n{m1 * 10}")
-print(f"Результат умножения на скаляр (аргумент справа):\n{10 * m1}")
+print(f"Результат m1 + m2:\n{m1 + m2}")
+print(f"Результат m1 * 10:\n{m1 * 10}")
+print(f"Результат 10 * m1:\n{10 * m1}")
 
 # C
 m2 = Matrix([[0, 1, 0], [20, 0, -1]])
@@ -89,4 +130,33 @@ print(f"Результат transpose():\n{m2}")
 
 m3 = Matrix.transposed(m2)
 print(f"Результат transposed():\n{m3}")
+
+# D - check 1
+mid = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+m1 = Matrix([[3, 2], [-10, 0], [14, 5]])
+m2 = Matrix([[5, 2, 10], [-0.5, -0.25, 18], [-22, -2.5, -0.125]])
+print(f"\nРезультат mid * m1:\n{mid * m1}")
+print(f"\nРезультат mid * m2:\n{mid * m2}")
+print(f"\nРезультат m2 * m1:\n{m2 * m1}")
+
+try:
+    m = m1 * m2
+    print("WA It should be error")
+except MatrixError as e:
+    print(f"\n{e.matrix1}\n")
+    print(e.matrix2)
+
+# D - check 2
+mid = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+m1 = Matrix([[3, 2], [-10, 0], [14, 5]])
+m2 = Matrix([[5, 2, 10], [-0.5, -0.25, 18], [-22, -2.5, -0.125]])
+print(f"\nРезультат 0.5 * m2:\n{0.5 * m2}")
+print(f"\nРезультат m2 * (0.5 * mid * m1):\n{m2 * (0.5 * mid * m1)}")
+
+# D - check 3
+mid = Matrix([[1, 0, 0],[0, 1, 0],[0, 0, 1]])
+m1 = Matrix([[3, 2], [-10, 0], [14, 5]])
+m2 = Matrix([[5, 2, 10], [-0.5, -0.25, 18], [-22, -2.5, -0.125]])
+print(f"\nРезультат 5 * m2:\n{5 * m2}")
+print(f"\nРезультат m2 * (120 * mid * m1):\n{m2 * (120 * mid * m1)}")
 
